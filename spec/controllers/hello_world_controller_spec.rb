f5a80c9a5be355a941875ce32f66b88ff533b85a
davidauza.engineer <davidauza01@gmail.com>
2021-02-12 09:13:25 -0500

    Add hello world controller tests

---
+++ spec/controllers/hello_world_controller_spec.rb
@@ -0,0 +1,11 @@
+describe 'HelloWorldController' do
+  tests HelloWorldController
+
+  it 'has a UILabel with correct text' do
+    label = controller.instance_variable_get('@label')
+    # check text
+    label.text.should.equal 'Hello World'
+    # make sure it is added on the view
+    controller.view.subviews.should.include? label
+  end
+end
