75f3811ea02f5975605053165f404a024441303d
davidauza.engineer <davidauza01@gmail.com>
2021-02-12 08:49:13 -0500

    Add initial specs

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,9 +1,18 @@
 describe "Application 'HelloWorld'" do
   before do
     @app = UIApplication.sharedApplication
+    @window = @app.windows[0]
   end
 
   it "has one window" do
     @app.windows.size.should == 1
+  end
+
+  it 'has a rootViewController' do
+    @window.rootViewController.should.not == nil
+  end
+
+  it 'uses HelloWorldController as the rootViewController' do
+    @window.rootViewController.class.should.equal UINavigationController
   end
 end
