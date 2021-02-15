3e15b67a5810754491735021477cff1979abbe94
davidauza.engineer <davidauza01@gmail.com>
2021-02-15 08:16:06 -0500

    Update hello_world_controller.rb

--- app/controllers/hello_world_controller.rb
+++ app/controllers/hello_world_controller.rb
@@ -3,10 +3,14 @@ class HelloWorldController < UIViewController
     p 'Hello World!'
 
     view.backgroundColor = UIColor.whiteColor
+    center_x = view.frame.size.width / 2
+    center_y = view.frame.size.height / 2
 
     @label = UILabel.alloc.init
     @label.text = 'Hello World'
-    @label.frame = CGRect.new([50, 50], [150, 50])
+    @label.sizeToFit
+    @label.center = [center_x, center_y]
+    
     view.addSubview(@label)
   end
 end
